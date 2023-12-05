a7722ba9d4a0781e71308e5e1d8662ff9c33014c
Oleg Putseiko <oleg.putseiko@gmail.com>
2023-12-06 02:18:36 +0400

    fix(plugin): replace default plugin export with named one of plugin properties

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ import { ESLint } from 'eslint';
 import { noDeniedLayersRule } from './rules/no-denied-layers';
 import { noUnknownLayersRule } from './rules/no-unknown-layers';
 
-const plugin: ESLint.Plugin = {
-  rules: {
-    'no-denied-layers': noDeniedLayersRule,
-    'no-unknown-layers': noUnknownLayersRule,
-  },
+export const rules: ESLint.Plugin['rules'] = {
+  'no-denied-layers': noDeniedLayersRule,
+  'no-unknown-layers': noUnknownLayersRule,
 };
-
-export default plugin;
