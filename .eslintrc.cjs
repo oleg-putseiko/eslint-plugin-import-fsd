0b8b0ea7baf27ed6869b8fa58cb7364b48018326
Oleg Putseiko <oleg.putseiko@gmail.com>
2023-12-06 01:30:46 +0400

    fix(lint): allow execution of husky hooks and add typescript eslint parser and plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,20 @@
  */
 module.exports = {
   root: true,
-  extends: ['prettier', 'eslint:recommended'],
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
   env: {
     node: true,
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    sourceType: 'module',
     ecmaVersion: 'latest',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
 };
