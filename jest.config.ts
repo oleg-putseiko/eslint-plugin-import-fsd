eb3b05610982c6136d7c72417b26467c4fe2a759
Oleg Putseiko <oleg.putseiko@gmail.com>
2024-02-08 23:54:58 +0400

    test(denied-layers): add aliased import tests from the app layer

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
   coveragePathIgnorePatterns: ['/node_modules/', 'src/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  verbose: true,
 };
 
 // eslint-disable-next-line no-restricted-exports
