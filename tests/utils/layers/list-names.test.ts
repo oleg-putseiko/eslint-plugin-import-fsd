046201857c51241254fc16978fde8b24de4e8003
Oleg Putseiko <oleg.putseiko@gmail.com>
2024-02-10 18:11:54 +0400

    test(utils): add layer utility tests

---
+++ tests/utils/layers/list-names.test.ts
@@ -0,0 +1,13 @@
+import { listNames } from '../../../src/utils/layers';
+
+describe('listNames', () => {
+  it('should return a string with a list of passed names', () => {
+    expect(listNames([])).toBe('');
+    expect(listNames(['foo'])).toBe("'foo'");
+    expect(listNames(['foo', 'bar'])).toBe("'foo' or 'bar'");
+    expect(listNames(['foo', 'bar', 'baz'])).toBe("'foo', 'bar' or 'baz'");
+    expect(listNames(['foo', 'bar', 'baz', 'qux'])).toBe(
+      "'foo', 'bar', 'baz' or 'qux'",
+    );
+  });
+});
