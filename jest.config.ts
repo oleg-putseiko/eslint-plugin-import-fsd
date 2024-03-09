13628ef91cc1e0cb7f1441a00b32383a48249940
Oleg Putseiko <oleg.putseiko@gmail.com>
2024-03-09 21:31:23 +0400

    chore(jest): collect test coverage of rules and configs from the build

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,18 @@
-import type { Config } from 'jest';
+import { type Config } from 'jest';
 
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
+  collectCoverageFrom: [
+    'dist/rules/**/*.{js,ts}',
+    'dist/index.{js,ts}',
+    'src/utils/**/*.{js,ts}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', 'src/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true,
 };
 
 // eslint-disable-next-line no-restricted-exports
