ccf2cb7e7b3892bd8bc22d98e14a9780d2c70e1e
Oleg Putseiko <oleg.putseiko@gmail.com>
2024-06-02 17:47:12 +1000

    test(context): remove unnecessary tests for aliases and overrides settings

--- tests/utils/rule/context/extract-path-context/aliases.test.ts
+++ tests/utils/rule/context/extract-path-context/aliases.test.ts
@@ -2,44 +2,7 @@ import { extractPathContext } from '../../../../../src/utils/rule/context';
 
 type RuleContext = Parameters<typeof extractPathContext>[0];
 
-class TestClass {}
-
 describe('aliases context property', () => {
-  it.each([
-    { aliases: null, type: 'null' },
-    { aliases: undefined, type: 'undefined' },
-  ])('should be empty object if it is $type', ({ aliases }) => {
-    const ruleContext: RuleContext = {
-      cwd: '/foo/bar',
-      filename: '/baz/qux',
-      settings: { fsd: { aliases } },
-    };
-
-    expect(extractPathContext(ruleContext)?.aliases).toEqual({});
-  });
-
-  it.each([
-    { aliases: {}, type: 'empty object' },
-    {
-      aliases: {
-        '@/src/*': './src/*',
-        '~/src/*': '/src/*',
-        fooBar: 'baz-qux',
-        '*': '*',
-        '': '',
-      },
-      type: 'object with string replacements',
-    },
-  ])('should be the same as passed if it is $type', ({ aliases }) => {
-    const ruleContext: RuleContext = {
-      cwd: '/foo/bar',
-      filename: '/baz/qux',
-      settings: { fsd: { aliases } },
-    };
-
-    expect(extractPathContext(ruleContext)?.aliases).toEqual(aliases);
-  });
-
   it.each([
     { aliases: 0, type: 'falsy number' },
     { aliases: 123, type: 'truthy number' },
@@ -72,11 +35,7 @@ describe('aliases context property', () => {
   );
 
   // TODO: remove `failing` and merge with the same tests
-  it.failing.each([
-    { aliases: new TestClass(), type: 'class instance' },
-    { aliases: ['foo', 'bar'], type: 'string array' },
-    { aliases: [], type: 'empty array' },
-  ])(
+  it.failing.each([{ aliases: ['foo', 'bar'], type: 'string array' }])(
     'context should be null if the aliases property from settings is $type',
     ({ aliases }) => {
       const ruleContext: RuleContext = {
--- tests/utils/rule/context/extract-path-context/overrides.test.ts
+++ tests/utils/rule/context/extract-path-context/overrides.test.ts
@@ -2,42 +2,7 @@ import { extractPathContext } from '../../../../../src/utils/rule/context';
 
 type RuleContext = Parameters<typeof extractPathContext>[0];
 
-class TestClass {}
-
 describe('overrides context property', () => {
-  it.each([
-    { overrides: null, type: 'null' },
-    { overrides: undefined, type: 'undefined' },
-  ])('should be empty object if it is $type', ({ overrides }) => {
-    const ruleContext: RuleContext = {
-      cwd: '/foo/bar',
-      filename: '/baz/qux.js',
-      settings: { fsd: { overrides } },
-    };
-
-    expect(extractPathContext(ruleContext)?.overrides).toEqual({});
-  });
-
-  it.each([
-    { overrides: {}, type: 'empty object' },
-    {
-      overrides: {
-        fooBar: { layer: 'foo', slice: 'bar' },
-        '123': { layer: 'baz', slice: 'qux' },
-        '': { layer: 'qux', slice: 'quux' },
-      },
-      type: 'object with assigned segments',
-    },
-  ])('should be the same as passed if it is $type', ({ overrides }) => {
-    const ruleContext: RuleContext = {
-      cwd: '/foo/bar',
-      filename: '/baz/qux.js',
-      settings: { fsd: { overrides } },
-    };
-
-    expect(extractPathContext(ruleContext)?.overrides).toEqual(overrides);
-  });
-
   it.each([
     { overrides: 0, type: 'falsy number' },
     { overrides: 123, type: 'truthy number' },
@@ -46,7 +11,7 @@ describe('overrides context property', () => {
     { overrides: false, type: 'false' },
     { overrides: Symbol(), type: 'symbol' },
     { overrides: () => 'fooBar', type: 'function' },
-    { overrides: ['foo', 'bar'], type: 'string array' },
+    { overrides: [{ layer: 'foo', slice: 'bar' }], type: 'array' },
     {
       overrides: { fooBar: { layer: 'foo' } },
       type: 'object without slice segment',
@@ -55,27 +20,12 @@ describe('overrides context property', () => {
       overrides: { fooBar: { slice: 'foo' } },
       type: 'object without layer segment',
     },
-    { overrides: [], type: 'empty array' },
   ])(
-    'context should be null if the overrides property from settings is $type',
+    'path context should be null if the overrides is $type',
     ({ overrides }) => {
       const ruleContext: RuleContext = {
-        cwd: '/foo/bar',
-        filename: '/baz/qux.js',
-        settings: { fsd: { overrides } },
-      };
-
-      expect(extractPathContext(ruleContext)).toBeNull();
-    },
-  );
-
-  // TODO: remove `failing` and merge with the same tests
-  it.failing.each([{ overrides: new TestClass(), type: 'class instance' }])(
-    'context should be null if the overrides property from settings is $type',
-    ({ overrides }) => {
-      const ruleContext: RuleContext = {
-        cwd: '/foo/bar',
-        filename: '/baz/qux.js',
+        cwd: '/',
+        filename: '/foo/bar.js',
         settings: { fsd: { overrides } },
       };
 
