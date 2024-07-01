69a321c34511e70b562637ee893ca29e2fd9b9b2
Oleg Putseiko <oleg.putseiko@gmail.com>
2024-07-02 02:55:12 +1000

    test(recommended-config): add flat recommended plugin config tests

--- tests/configs/recommended-legacy.test.ts
+++ tests/configs/recommended-legacy.test.ts
@@ -2,17 +2,17 @@ import plugin from '../../dist/index';
 
 const { configs } = plugin;
 
-describe('recommended config', () => {
+describe('recommended-legacy config', () => {
   it('should have three rules', () => {
-    const rules = configs.recommended.rules ?? {};
+    const rules = configs['recommended-legacy'].rules ?? {};
 
     expect(Object.keys(rules)).toHaveLength(3);
   });
 
   it('should have plugin as a dependency', () => {
-    const rules = configs.recommended.plugins ?? {};
+    const rules = configs['recommended-legacy'].plugins ?? [];
 
-    expect(rules?.['import-fsd']).toEqual(plugin);
+    expect(rules).toContain('import-fsd');
   });
 
   it.each([
@@ -20,7 +20,7 @@ describe('recommended config', () => {
     { rule: 'import-fsd/no-deprecated-layers' },
     { rule: 'import-fsd/no-unknown-layers' },
   ])('should have rule $rule', ({ rule }) => {
-    expect(configs.recommended.rules).toHaveProperty(rule);
+    expect(configs['recommended-legacy'].rules).toHaveProperty(rule);
   });
 
   it.each([
@@ -28,7 +28,9 @@ describe('recommended config', () => {
     { rule: 'import-fsd/no-deprecated-layers' },
     { rule: 'import-fsd/no-unknown-layers' },
   ])('rule $rule should not have options', ({ rule }) => {
-    expect(Array.isArray(configs.recommended.rules?.[rule])).toBe(false);
+    expect(Array.isArray(configs['recommended-legacy'].rules?.[rule])).toBe(
+      false,
+    );
   });
 
   it.each([
@@ -36,6 +38,6 @@ describe('recommended config', () => {
     { rule: 'import-fsd/no-deprecated-layers', severity: 'warn' },
     { rule: 'import-fsd/no-unknown-layers', severity: 'error' },
   ])('rule $rule should have $severity severity', ({ rule, severity }) => {
-    expect(configs.recommended.rules?.[rule]).toEqual(severity);
+    expect(configs['recommended-legacy'].rules?.[rule]).toEqual(severity);
   });
 });
