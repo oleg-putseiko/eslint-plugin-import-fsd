600affc524b6668e792f057caa067d8bea7f1e35
Oleg Putseiko <oleg.putseiko@gmail.com>
2024-08-14 03:57:47 +1000

    fix(aliases): escape regexp special characters in path patterns

--- src/utils/rule/aliases.ts
+++ src/utils/rule/aliases.ts
@@ -2,6 +2,22 @@ import { isObject, isString } from '../guards';
 
 export type Aliases = { [alias: string]: string };
 
+const ESCAPED_CHARACTERS: string[] = [
+  '\\',
+  '[',
+  ']',
+  '{',
+  '}',
+  '(',
+  ')',
+  '^',
+  '$',
+  '.',
+  '|',
+  '?',
+  '+',
+];
+
 export const isAliases = (value: unknown): value is Aliases =>
   isObject(value) &&
   !Array.isArray(value) &&
@@ -11,7 +27,11 @@ export const resolveAliasedPath = (aliases: Aliases, path: string) => {
   const patterns = Object.keys(aliases);
 
   for (const pattern of patterns) {
-    const replacement = RegExp(`^${pattern.replace('*', '(.*)')}$`, 'iu')
+    const escapedPattern = ESCAPED_CHARACTERS.reduce(
+      (acc, character) => acc.replaceAll(character, `\\${character}`),
+      pattern,
+    );
+    const replacement = RegExp(`^${escapedPattern.replace('*', '(.*)')}$`, 'iu')
       .exec(path)
       ?.at(1);
 
